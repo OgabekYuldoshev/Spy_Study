c6266d5a385196f2ab42b91d0ca5664914adc392
OgabekYuldoshev <yuldashoff1@gmail.com>
2021-02-23 23:36:42 +0500

    g

--- server/server.js
+++ server/server.js
@@ -2,16 +2,13 @@ const http = require("http")
 const express = require("express");
 const helmet = require("helmet")
 const morgan = require("morgan")
-const mongoose = require("mongoose");
-const MsgSchema = require("./model/schema")
 
 
-//Connected to DB
-const db = mongoose.connect("mongodb+srv://Anonymous:ogabek2001@cluster0.uz1y4.mongodb.net/chatMassages?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>console.log("Connected")).catch((error)=>(console.error(error)))
+
 
 const app = express();
 const server = http.createServer(app)
-const io = require("socket.io")(server)
+
 
 
 
@@ -20,38 +17,13 @@ app.use(express.static("public"))
 app.use(helmet())
 app.use(morgan("combined"));
 
+app.get("/iakt", (req, res)=>{
+    res.json(data)
+})
+
 
 
 //Express technology
-
-
-
-const users = {}
-
-//Socket
-io.on("connection", socket=>{
-    socket.on("joined-user", name=>{
-        users[socket.id] = name
-        socket.broadcast.emit("newUser", name)
-    })
-    socket.on("disconnect", ()=>{
-        socket.broadcast.emit("user-disconected", users[socket.id])
-        delete users[socket.id]
-    })
-    socket.on("send-massage", msg=>{
-        socket.broadcast.emit("massage", {
-            massage: msg,
-            username: users[socket.id]
-        })
-    })
-})
-io.on("disconnect", socket=>{
-    console.log(socket)
-})
-
-
-
-
 
 const PORT = process.env.PORT || 8000
 
